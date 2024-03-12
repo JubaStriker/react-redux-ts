ab07d728aa78019b0f7ec774639d85979dfa747f
JubaStriker <jubastriker@gmail.com>
2024-03-13 03:02:02 +0600

    Modal and Dropdown added

--- src/components/todo/AddTodoModal.tsx
+++ src/components/todo/AddTodoModal.tsx
@@ -1,9 +1,48 @@
+import { Button } from "@/components/ui/button"
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
 
 const AddTodoModal = () => {
     return (
-        <div>
-
-        </div>
+        <Dialog>
+            <DialogTrigger asChild>
+                <Button className="bg-primary-gradient text-lg font-semibold">Add todo</Button>
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-[425px]">
+                <DialogHeader>
+                    <DialogTitle>Add Task</DialogTitle>
+                    <DialogDescription>
+                        Add your task that you want to finish
+                    </DialogDescription>
+                </DialogHeader>
+                <div className="grid gap-4 py-4">
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <Label htmlFor="name" className="text-right">
+                            Name
+                        </Label>
+                        <Input id="name" value="Pedro Duarte" className="col-span-3" />
+                    </div>
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <Label htmlFor="username" className="text-right">
+                            Username
+                        </Label>
+                        <Input id="username" value="@peduarte" className="col-span-3" />
+                    </div>
+                </div>
+                <DialogFooter>
+                    <Button type="submit">Save changes</Button>
+                </DialogFooter>
+            </DialogContent>
+        </Dialog>
     );
 };
 
--- src/components/todo/TodoContainer.tsx
+++ src/components/todo/TodoContainer.tsx
@@ -1,12 +1,13 @@
-import { Button } from "../ui/button";
+import AddTodoModal from "./AddTodoModal";
 import TodoCard from "./TodoCard";
+import TodoFilter from "./TodoFilter";
 
 const TodoContainer = () => {
     return (
         <div>
             <div className="flex justify-between mb-5">
-                <Button className="bg-primary-gradient text-xl font-semibold">Add todo</Button>
-                <Button className="bg-primary-gradient text-xl font-semibold">Filter</Button>
+                <AddTodoModal />
+                <TodoFilter />
             </div>
             <div className="bg-primary-gradient w-full h-full rounded-lg p-4 space-y-3">
                 <TodoCard />
